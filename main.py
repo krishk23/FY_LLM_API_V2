4673b34d4b419635a4ad0bcdb44e80cacdb2a580
Krishna Kolekar <146716645+krishk23@users.noreply.github.com>
2024-11-21 22:22:05 +0530

    Update main.py updated for new api key

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from fastapi.middleware.cors import CORSMiddleware
 load_dotenv()
 
 # Configure the API key for generative AI
-genai.configure(api_key=("AIzaSyARLRWGr4vloCAhea-rt8dMV7gWeC1pgBE")) 
+genai.configure(api_key=("AIzaSyC0E5Tcbp3V6LXmw0ZuD2ih_nJkBronHhw")) 
 
 # Initialize FastAPI
 app = FastAPI()
